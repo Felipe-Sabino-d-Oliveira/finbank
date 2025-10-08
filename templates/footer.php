b38a53cd13e9ff9b61367c886943e83717ee8ae9
F <f>
2025-10-08 10:10:51 -0300

    atualizado: estilos;

--- templates/footer.php
+++ templates/footer.php
@@ -1,5 +1,6 @@
 </div>
-<footer class="bg-dark text-white text-center py-3 mt-5">
+<!-- footer.php -->
+<footer class="bg-dark text-white text-center py-3 mt-auto">
     <p>&copy; <?= date('Y') ?> FinBank. Todos os direitos reservados.</p>
 </footer>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
